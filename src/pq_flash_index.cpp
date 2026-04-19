9c2819cc5b60ffea2056b36eeb75e669c72a4de8
fogman <zhanglin20050530@163.com>
2026-04-19 12:17:14 +0800

    modify diskann cache_num_nodes

--- src/pq_flash_index.cpp
+++ src/pq_flash_index.cpp
@@ -536,11 +536,12 @@ void PQFlashIndex<T, LabelT>::cache_bfs_levels(uint64_t num_nodes_to_cache, std:
     tsl::robin_set<uint32_t> node_set;
 
     // Do not cache more than 10% of the nodes in the index
-    uint64_t tenp_nodes = (uint64_t)(std::round(this->_num_points * 0.1));
+    // 对压缩版 DiskANN，可以缓存超过10%的节点，因为每个节点占用的空间更小；但对于非压缩版，缓存过多节点可能会占用过多内存，因此限制在10%以内。
+    uint64_t tenp_nodes = (uint64_t)(std::round(this->_num_points * 0.5));
     if (num_nodes_to_cache > tenp_nodes)
     {
         diskann::cout << "Reducing nodes to cache from: " << num_nodes_to_cache << " to: " << tenp_nodes
-                      << "(10 percent of total nodes:" << this->_num_points << ")" << std::endl;
+                      << "(50 percent of total nodes:" << this->_num_points << ")" << std::endl;
         num_nodes_to_cache = tenp_nodes == 0 ? 1 : tenp_nodes;
     }
     diskann::cout << "Caching " << num_nodes_to_cache << "..." << std::endl;
@@ -1525,7 +1526,7 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
     uint8_t *pq_coord_scratch = pq_query_scratch->aligned_pq_coord_scratch;
 
     // lambda to batch compute query<-> node distances in PQ space
-    // 定义了一�?lambda 函数用于在内存中利用 PQ 表快速批量计算点�?query 的距�?
+    // 定义了一�?lambda 函数用于在内存中利用 PQ 表快速批量计算点�?query 的距�?
     auto compute_dists = [this, pq_coord_scratch, pq_dists](const uint32_t *ids, const uint64_t n_ids,
                                                             float *dists_out) {
         diskann::aggregate_coords(ids, n_ids, this->data, this->_n_chunks, pq_coord_scratch);
@@ -1538,7 +1539,7 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
     retset.reserve(l_search);
     std::vector<Neighbor> &full_retset = query_scratch->full_retset;
 
-    // 找到距离 query 最近的 medoid 作为 beam search 的起�?
+    // 找到距离 query 最近的 medoid 作为 beam search 的起�?
     uint32_t best_medoid = 0;
     float best_dist = (std::numeric_limits<float>::max)();
     if (!use_filter)
@@ -1612,7 +1613,7 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
         {
             auto nbr = retset.closest_unexpanded();
             num_seen++;
-            // 如果邻居在缓存中，则直接加入待处理的缓存列表；否则加�?frontier 列表，等待后续批�?IO 读取
+            // 如果邻居在缓存中，则直接加入待处理的缓存列表；否则加�?frontier 列表，等待后续批�?IO 读取
             auto iter = _nhood_cache.find(nbr.id);
             if (iter != _nhood_cache.end())
             {
@@ -1731,13 +1732,13 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
             char *node_disk_buf = offset_to_node(frontier_nhood.second, frontier_nhood.first);
             uint32_t *node_buf = offset_to_node_nhood(node_disk_buf);
             uint64_t nnbrs = (uint64_t)(*node_buf);
-            // �?node 的坐标数据从磁盘格式解压到内存格式，准备计算距离
+            // �?node 的坐标数据从磁盘格式解压到内存格式，准备计算距离
             T *node_fp_coords = offset_to_node_coords(node_disk_buf);
             memcpy(data_buf, node_fp_coords, _disk_bytes_per_point);
             float cur_expanded_dist;
             if (!_use_disk_index_pq)
             {
-                // 利用 _dist_cmp 计算 node �?query 的距�?
+                // 利用 _dist_cmp 计算 node �?query 的距�?
                 cur_expanded_dist = _dist_cmp->compare(aligned_query_T, data_buf, (uint32_t)_aligned_dim);
             }
             else
@@ -1747,7 +1748,7 @@ void PQFlashIndex<T, LabelT>::cached_beam_search(const T *query1, const uint64_t
                 else
                     cur_expanded_dist = _disk_pq_table.l2_distance(query_float, (uint8_t *)data_buf);
             }
-            // 将包含精确距离的节点推入最终候�?
+            // 将包含精确距离的节点推入最终候�?
             full_retset.push_back(Neighbor(frontier_nhood.first, cur_expanded_dist));
             uint32_t *node_nbrs = (node_buf + 1);
             // compute node_nbrs <-> query dist in PQ space
