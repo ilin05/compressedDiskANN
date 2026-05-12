be8320f33fa02471ffaa3fe348e331fca930fc61
fogman <zhanglin20050530@163.com>
2026-05-12 14:45:00 +0800

    add build all vamana index script

--- scripts/build_vamana_all_datasets.sh
+++ scripts/build_vamana_all_datasets.sh
@@ -16,4 +16,4 @@ DEEP_DIR="$BUILD_DIR/data/deep"
 # "$BUILD_DIR/apps/build_memory_index" --data_type float --dist_fn l2 --data_path $MNIST_DIR/mnist_base.fbin --index_path_prefix $MNIST_DIR/memory_index_mnist_60K_R32_L50_A1.2 -R 32 -L 50 -T 32 --alpha 1.2
 # "$BUILD_DIR/apps/build_memory_index" --data_type float --dist_fn l2 --data_path $GIST_DIR/gist_base.fbin --index_path_prefix $GIST_DIR/memory_index_gist_1M_R32_L50_A1.2 -R 32 -L 50 -T 32 --alpha 1.2
 # "$BUILD_DIR/apps/build_memory_index" --data_type float --dist_fn l2 --data_path $FASHION_DIR/fashion-mnist_base.fbin --index_path_prefix $FASHION_DIR/memory_index_fashion-mnist_60K_R32_L50_A1.2 -R 32 -L 50 -T 32 --alpha 1.2
-"$BUILD_DIR/apps/build_memory_index" --data_type float --dist_fn cosine --data_path $DEEP_DIR/deep_base.fbin --index_path_prefix $DEEP_DIR/memory_index_deep_10M_R32_L50_A1.2 -R 32 -L 50 -T 32 --alpha 1.2
+"$BUILD_DIR/apps/build_memory_index" --data_type float --dist_fn l2 --data_path $DEEP_DIR/deep_base.fbin --index_path_prefix $DEEP_DIR/memory_index_deep_10M_R32_L50_A1.2 -R 32 -L 50 -T 32 --alpha 1.2
