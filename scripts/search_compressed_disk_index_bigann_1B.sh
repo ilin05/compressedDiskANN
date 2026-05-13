e700a93c388dd4656d7e11e5209e8c51ad3e5df0
fogman <zhanglin20050530@163.com>
2026-05-13 23:37:19 +0800

    modify test rounds

--- scripts/search_compressed_disk_index_bigann_1B.sh
+++ scripts/search_compressed_disk_index_bigann_1B.sh
@@ -21,6 +21,6 @@ DATA_DIR="$BUILD_DIR/data/bigann"
   --index_path_prefix "$DATA_DIR/disk_index_bigann_1B_R32_L50_A1.2" \
   --query_file "$DATA_DIR/bigann_query.fbin" \
   --gt_file "$DATA_DIR/bigann_1B_gt.bin" \
-  -K 10 -L 1 2 3 4 5 6 8 10 11 12 13 14 16 18 20 -T 1 \
+  -K 10 -L 10 11 12 13 14 16 18 20 -T 1 \
   --result_path "$DATA_DIR/res" \
   --num_nodes_to_cache 400000000
--- scripts/search_compressed_memory_index_bigann_100M.sh
+++ scripts/search_compressed_memory_index_bigann_100M.sh
@@ -24,7 +24,7 @@ DATA_DIR="$BUILD_DIR/data/bigann"
   --index_path_prefix "$DATA_DIR/compressed_memory_index_bigann_100M_R32_L50_A1.2" \
   --gt_file "$DATA_DIR/bigann_100M_gt.bin" \
   --query_file "$DATA_DIR/bigann_query.fbin" \
-  -K 10 -L 1 2 3 4 5 6 8 10 11 12 13 14 16 18 20 -T 1 \
+  -K 10 -L 10 11 12 13 14 16 18 20 -T 1 \
   --result_path "$DATA_DIR/res" \
   --use_pq_dist 1 \
   --pq_bytes 32 \
